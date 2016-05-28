212e4ef493bd5e8e220fd6b147100baf5b32f85d
Cezary Sałbut <csalbut@gmail.com>
2016-05-28 14:26:52 +0200

    Add emacs config for linux kernel coding style

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+((nil .
+      ((evil-shift-width . 8)
+       (tab-width . 8)
+       (indent-tabs-mode . t)
+       (evil-indent-convert-tabs . nil)))
+ (c-mode .
+         ((c-file-style . "linux"))))
