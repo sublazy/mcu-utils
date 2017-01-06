20aebd6bb09f0002f9007fad1879c9681befc1c1
Cezary Sałbut <csalbut@gmail.com>
2017-01-06 04:05:07 +0100

    add platform-specific LED code generation

--- src/led_zl31.c
+++ src/led_zl31.c
@@ -1,12 +1,22 @@
-// TODO this file should be generated from zl31.xml
-#include "led.h"
-#include "led_zl31.h"
+/* Copyright (c) 2016-2017 Cezary Salbut                              */
+/*                                                                    */
+/* This file is subject to the terms of the MIT license. If a copy of */
+/* the MIT license was not distributed with this file, You can obtain */
+/* one at https://opensource.org/licenses/MIT.                        */
+/*                                                                    */
+
+/* ********************** AUTO-GENERATED CODE **********************  */
+/* NOTE: This file has been generated automatically. To re-generate   */
+/* it, run 'make code'. You can edit this file manually to try out    */
+/* things quickly; for permanent changes the following files should   */
+/* be modified:                                                       */
+// TODO: List the files used for generation.
+
+#include "headers_all.h"
 
 void led_platform_init(void)
 {
-	bool active_low = true;
-	led_new(GPIOB, GPIO1, active_low);
-	led_new(GPIOB, GPIO2, active_low);
+        bool active_low;
+        led_new(GPIOB, GPIO1, active_low = true);
+        led_new(GPIOB, GPIO2, active_low = true);
 }
-
-//led_t *
