d6d11da7d38ca0aa05bd2473c7a103fa2ea8a9f9
Cezary Sałbut <csalbut@gmail.com>
2017-01-06 02:53:12 +0100

    add LED header generation

--- include/led_zl31.h
+++ include/led_zl31.h
@@ -1,19 +1,25 @@
-/*
- * Copyright (c) 2016, 2017 Cezary Sałbut
- *
- * This file is subject to the terms of the MIT license. If a copy of
- * the MIT license was not distributed with this file, You can obtain
- * one at https://opensource.org/licenses/MIT.
- */
+/* Copyright (c) 2016-2017 Cezary Salbut                              */
+/*                                                                    */
+/* This file is subject to the terms of the MIT license. If a copy of */
+/* the MIT license was not distributed with this file, You can obtain */
+/* one at https://opensource.org/licenses/MIT.                        */
+/*                                                                    */
+
+/* ********************** AUTO-GENERATED CODE **********************  */
+/* NOTE: This file has been generated automatically. To re-generate   */
+/* it, run 'make code'. You can edit this file manually to try out    */
+/* things quickly; for permanent changes the following files should   */
+/* be modified:                                                       */
+// TODO: List the files used for generation.
 
 #ifndef LED_ZL31_H
 #define LED_ZL31_H
 
 typedef enum led_id_zl31 {
-	LED1,
-	LED2,
-	NOF_LEDS
-}led_id_t;
+        LED1,
+        LED2,
+        NOF_LEDS
+} led_id_t;
 
 void led_platform_init(void);
 
